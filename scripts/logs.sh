53a7a803ae188c3cce9c67bc5f535d7f9cb8fb96
seba3011 <slopezalarcon10680@gmail.com>
2025-11-30 00:46:00 -0300

    tarea 3.6.8

--- scripts/logs.sh
+++ scripts/logs.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-    exit 1
-fi
+if [ -z "$1" ]; then exit 1; fi
 
 ID=$1
 LOG_FILE="logs/node${ID}.log"
 
 if [ -f "$LOG_FILE" ]; then
-    tail -f "$LOG_FILE"
+    tail -F "$LOG_FILE"
 fi
